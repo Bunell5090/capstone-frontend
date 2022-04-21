d7d420e3cd06f93afcb71d173671b4d0b08b20fc
Bunell5090 <Bunell5090.com>
2022-04-20 20:20:44 -0500

    netlify setup

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
+import BootstrapVue from "bootstrap-vue/dist/bootstrap-vue.esm";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+createApp(App).use(BootstrapVue);
 
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
 createApp(App).use(router).mount("#app");
+
+import "bootstrap/dist/js/bootstrap.js";
