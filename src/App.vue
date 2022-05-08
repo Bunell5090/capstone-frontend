087857d496a8b8898a52c91f568c795768a60392
Bunell5090 <Bunell5090.com>
2022-05-08 14:07:47 -0500

    Added Pets tab & removed unneeded tabs from main page

--- src/App.vue
+++ src/App.vue
@@ -242,14 +242,14 @@
   <aside id="sidebar" class="sidebar">
     <ul class="sidebar-nav" id="sidebar-nav">
       <li class="nav-item">
-        <a class="nav-link collapsed" href="index.html">
+        <a class="nav-link collapsed" href="/home">
           <i class="bi bi-grid"></i>
           <span>Dashboard</span>
         </a>
       </li>
       <!-- End Dashboard Nav -->
 
-      <li class="nav-item">
+      <!-- <li class="nav-item">
         <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
           <i class="bi bi-menu-button-wide"></i>
           <span>Components</span>
@@ -257,94 +257,94 @@
         </a>
         <ul id="components-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
           <li>
-            <a href="components-alerts.html">
+            <a href="/components-alerts.html">
               <i class="bi bi-circle"></i>
               <span>Alerts</span>
             </a>
           </li>
           <li>
-            <a href="components-accordion.html">
+            <a href="/components-accordion.html">
               <i class="bi bi-circle"></i>
               <span>Accordion</span>
             </a>
           </li>
           <li>
-            <a href="components-badges.html">
+            <a href="/components-badges.html">
               <i class="bi bi-circle"></i>
               <span>Badges</span>
             </a>
           </li>
           <li>
-            <a href="components-breadcrumbs.html">
+            <a href="/components-breadcrumbs.html">
               <i class="bi bi-circle"></i>
               <span>Breadcrumbs</span>
             </a>
           </li>
           <li>
-            <a href="components-buttons.html">
+            <a href="/components-buttons.html">
               <i class="bi bi-circle"></i>
               <span>Buttons</span>
             </a>
           </li>
           <li>
-            <a href="components-cards.html">
+            <a href="/components-cards.html">
               <i class="bi bi-circle"></i>
               <span>Cards</span>
             </a>
           </li>
           <li>
-            <a href="components-carousel.html">
+            <a href="/components-carousel.html">
               <i class="bi bi-circle"></i>
               <span>Carousel</span>
             </a>
           </li>
           <li>
-            <a href="components-list-group.html">
+            <a href="/components-list-group.html">
               <i class="bi bi-circle"></i>
               <span>List group</span>
             </a>
           </li>
           <li>
-            <a href="components-modal.html">
+            <a href="/components-modal.html">
               <i class="bi bi-circle"></i>
               <span>Modal</span>
             </a>
           </li>
           <li>
-            <a href="components-tabs.html">
+            <a href="/components-tabs.html">
               <i class="bi bi-circle"></i>
               <span>Tabs</span>
             </a>
           </li>
           <li>
-            <a href="components-pagination.html">
+            <a href="/components-pagination.html">
               <i class="bi bi-circle"></i>
               <span>Pagination</span>
             </a>
           </li>
           <li>
-            <a href="components-progress.html">
+            <a href="/components-progress.html">
               <i class="bi bi-circle"></i>
               <span>Progress</span>
             </a>
           </li>
           <li>
-            <a href="components-spinners.html">
+            <a href="/components-spinners.html">
               <i class="bi bi-circle"></i>
               <span>Spinners</span>
             </a>
           </li>
           <li>
-            <a href="components-tooltips.html">
+            <a href="/components-tooltips.html">
               <i class="bi bi-circle"></i>
               <span>Tooltips</span>
             </a>
           </li>
         </ul>
-      </li>
+      </li> -->
       <!-- End Components Nav -->
 
-      <li class="nav-item">
+      <!-- <li class="nav-item">
         <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
           <i class="bi bi-journal-text"></i>
           <span>Forms</span>
@@ -376,10 +376,10 @@
             </a>
           </li>
         </ul>
-      </li>
+      </li> -->
       <!-- End Forms Nav -->
 
-      <li class="nav-item">
+      <!-- <li class="nav-item">
         <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
           <i class="bi bi-layout-text-window-reverse"></i>
           <span>Tables</span>
@@ -399,10 +399,10 @@
             </a>
           </li>
         </ul>
-      </li>
+      </li> -->
       <!-- End Tables Nav -->
 
-      <li class="nav-item">
+      <!-- <li class="nav-item">
         <a class="nav-link collapsed" data-bs-target="#charts-nav" data-bs-toggle="collapse" href="#">
           <i class="bi bi-bar-chart"></i>
           <span>Charts</span>
@@ -428,10 +428,10 @@
             </a>
           </li>
         </ul>
-      </li>
+      </li> -->
       <!-- End Charts Nav -->
 
-      <li class="nav-item">
+      <!-- <li class="nav-item">
         <a class="nav-link collapsed" data-bs-target="#icons-nav" data-bs-toggle="collapse" href="#">
           <i class="bi bi-gem"></i>
           <span>Icons</span>
@@ -457,10 +457,16 @@
             </a>
           </li>
         </ul>
-      </li>
+      </li> -->
       <!-- End Icons Nav -->
 
-      <li class="nav-heading">Pages</li>
+      <!-- <li class="nav-heading">Pages</li> -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="/pets">
+          <i class="bx bxl-baidu"></i>
+          <span>Pets</span>
+        </a>
+      </li>
 
       <li class="nav-item">
         <a class="nav-link collapsed" href="users-profile.html">
@@ -470,24 +476,24 @@
       </li>
       <!-- End Profile Page Nav -->
 
-      <li class="nav-item">
+      <!-- <li class="nav-item">
         <a class="nav-link collapsed" href="pages-faq.html">
           <i class="bi bi-question-circle"></i>
           <span>F.A.Q</span>
         </a>
-      </li>
+      </li> -->
       <!-- End F.A.Q Page Nav -->
 
-      <li class="nav-item">
+      <!-- <li class="nav-item">
         <a class="nav-link collapsed" href="pages-contact.html">
           <i class="bi bi-envelope"></i>
           <span>Contact</span>
         </a>
-      </li>
+      </li> -->
       <!-- End Contact Page Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="register">
+        <a class="nav-link collapsed" href="/register">
           <i class="bi bi-card-list"></i>
           <span>Register</span>
         </a>
@@ -495,27 +501,27 @@
       <!-- End Register Page Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-login.html">
+        <a class="nav-link collapsed" href="/login">
           <i class="bi bi-box-arrow-in-right"></i>
           <span>Logout</span>
         </a>
       </li>
       <!-- End Login Page Nav -->
 
-      <li class="nav-item">
+      <!-- <li class="nav-item">
         <a class="nav-link collapsed" href="pages-error-404.html">
           <i class="bi bi-dash-circle"></i>
           <span>Error 404</span>
         </a>
-      </li>
+      </li> -->
       <!-- End Error 404 Page Nav -->
 
-      <li class="nav-item">
+      <!-- <li class="nav-item">
         <a class="nav-link" href="pages-blank.html">
           <i class="bi bi-file-earmark"></i>
           <span>Blank</span>
         </a>
-      </li>
+      </li> -->
       <!-- End Blank Page Nav -->
     </ul>
   </aside>
